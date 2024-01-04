60f59b140fa576398585627cefca762b676d6747
Valentin Thibault <valentin.thlt@gmail.com>
2024-01-04 10:10:48 +0100

    feat: create Identite class

---
+++ src/main/java/m2i/valentin/tp2/classes/Identite.java
@@ -0,0 +1,31 @@
+package m2i.valentin.tp2.classes;
+
+public class Identite {
+    private String nom;
+    private String prenom;
+    private Adresse adresse;
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return this.prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Adresse getAdresse() {
+        return this.adresse;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
+}
